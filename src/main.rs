0506c61026081c9d4ba7304b860f23c2ddcb0102
Gaurav Sablok <gauravcodepro@gmail.com>
2025-01-16 20:30:06 +0100

    sciencegenome
    started to finalize this restapi and prepairing for the final release.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 mod args;
 mod snp;
 use crate::args::PersonalArgs;
-use crate::snp::PersonalFasta;
 use crate::snp::PersonalGenome;
-use rocket::*;
-use rocket_contrib::templates::Template;
-use serde::Serialize;
-use rocket::reponse::content::Json;
-use rocket::request::form;
 use rocket::Request;
+use rocket::get;
+use rocket::routes;
+use rocket::form;
+use serde::Serialize;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
@@ -28,55 +26,47 @@ use std::io::{BufRead, BufReader, Write};
 */
 
 async fn main() {
-    rocket::build().mount("/", routes![personalgenome]).launch.await;
+    rocket::build()
+        .mount("/", routes![personalgenome])
+        .launch()
+        .await;
 }
 
 /*
  * roter function for the personal genome snp profile.
  * */
 #[get("/rsidpath")]
-async fn personalgenome(
-    rsidpath: &str,
-) -> Result<String, Box<dyn Error>> {
-    let args = OpenSNPArgs::parse();
+async fn personalgenome(rsidpath: &str) -> Result<String, Box<dyn Error>> {
     let personalgenomeopen = File::open("personalgenome.file").expect("file not found");
-    let upperbase: usize = &args.upper.unwrap();
-    let lowerbase: usize = &args.lower.unwrap();
     let personalgenomeread = BufReader::new(personalgenomeopen);
     let mut personalgenome: Vec<PersonalGenome> = Vec::new();
 
-
     for i in personalgenomeread.lines() {
-        for j in fasta_personal_unwrap.iter() {
-            let line = i.expect("line not found");
-            let personalmutable: Vec<_> = line.split("\t").collect::<Vec<_>>();
-            let genotypesplit: Vec<String, String> = personalmutable[3]
-                .split(|c: char| !c.is_numeric())
-                .filter(|x| !x.is_empty())
-                .collect::<Vec<_>>();
-            if j.header == personalmutable[1].parse::<usize>().unwrap() {
-                personalgenome.push(PersonalGenome {
-                    rsid: personalmutable[0].to_string(),
-                    chromosome: personalmutable[1],
-                    position: personalmutable[2].parse::<usize>().unwrap(),
-                    genotype: personalmutable[3].to_string(),
-                    indivisualsnp: genotypesplit[0],
-                    humansnp: genotypesplit[1],
-                });
-            }
-        }
+        let line = i.expect("line not found");
+        let personalmutable: Vec<_> = line.split("\t").collect::<Vec<_>>();
+        let genotypesplit: Vec<String, String> = personalmutable[3]
+            .split(|c: char| !c.is_numeric())
+            .filter(|x| !x.is_empty())
+            .collect::<Vec<_>>();
+        personalgenome.push(PersonalGenome {
+            rsid: personalmutable[0].to_string(),
+            chromosome: personalmutable[1].parse::<usize>().unwrap(),
+            position: personalmutable[2].parse::<usize>().unwrap(),
+            genotype: personalmutable[3].to_string(),
+            indivisualsnp: genotypesplit[0].to_string(),
+            humansnp: genotypesplit[1].to_string(),
+        });
     }
 
-    for i in personalgenome.iter(){
-      if rsidpath == i.rsid {
-         println!("rsid queried:{}", i.rsid);
-         println!("chromosome:{}", i.chromosome);
-         println!("position:{}", i.position);
-         println!("genotype:{}", i.genotype);
-         println!("indiviualsnp:{}", i.indivisualsnp);
-         println!("humansnp:{}", i.humansnp);
-      }
-
+    for i in personalgenome.iter() {
+        if rsidpath == i.rsid {
+            println!("rsid queried:{}", i.rsid);
+            println!("chromosome:{}", i.chromosome);
+            println!("position:{}", i.position);
+            println!("genotype:{}", i.genotype);
+            println!("indiviualsnp:{}", i.indivisualsnp);
+            println!("humansnp:{}", i.humansnp);
+        }
     }
 
     Ok("personal genome string".to_string())
